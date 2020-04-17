5f7669de36c4aee3d6de5a8b3c143e5cf5194124
djutras2 <dustin.jutras@gmail.com>
2020-04-17 11:01:47 -0400

    St Louis scrape

---
+++ State Scrapes/st louis.py
@@ -0,0 +1,64 @@
+# https://www.stlouis-mo.gov/covid-19/data/zip.cfm
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from datetime import datetime
+import os
+
+import csv
+
+from state_helper import header, is_int
+
+def fetch():
+    location = "STL.csv"
+
+    try:
+        url = 'https://www.stlouis-mo.gov/covid-19/data/zip.cfm'
+
+        options = webdriver.ChromeOptions()
+        options.add_argument('--ignore-certificate-errors')
+        options.add_argument('--incognito')
+        options.add_argument('--headless')
+        options.add_argument("--disable-infobars")
+        driver = webdriver.Chrome("./chromedriver.exe", options=options)
+        
+        driver.get(url)
+        
+        # print(driver.page_source)
+
+        driver.implicitly_wait(10)
+
+        # get table body
+        table = driver.find_element_by_xpath('//*[@id="CS_CCF_812712_812687"]/div/div[4]/div[2]/div/table/tbody')
+
+        table_html = table.get_attribute('innerHTML')
+
+        # print(table_html)
+
+        driver.quit()
+
+    except RuntimeError as e:
+        print(url + " failed to load")
+        print(e)
+        driver.quit()
+        return None
+    
+    soup = BeautifulSoup(table_html, "html.parser")
+
+    with open(location, 'w', encoding='utf-8') as out:
+        writer = csv.writer(out)
+        writer.writerow(header)
+        for tag in soup.find_all("tr"):
+            tds = tag.find_all("td")
+            zip = tds[0].getText().strip()
+            cases = tds[1].getText().strip()
+
+            if(is_int(zip)):
+                writer.writerow([zip, cases, "N/A", datetime.date(datetime.now()), url])
+
+fetch()
