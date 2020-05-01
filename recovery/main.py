d576b67aa774b086c5102ec8efe01713f1c8b7c2
djutras2 <dustin.jutras@gmail.com>
2020-05-01 15:43:56 -0400

    Worked days of imporvement formula

--- recovery/main.py
+++ recovery/main.py
@@ -10,6 +10,12 @@ test = False
 
 directory = ""
 
+def get_pop(pop_data, fips):
+    for row in pop_data:
+        if fips == row[0]:
+            return float(row[3])
+    return None
+
 def download_document(document_url, file_name):
     response = urlopen(document_url)
     file = open(file_name, 'wb')
@@ -23,6 +29,7 @@ def main():
     usafact_cases_data_url = "https://usafactsstatic.blob.core.windows.net/public/data/covid-19/covid_confirmed_usafacts.csv"
     usafact_cases_filename = "usafact_cases.csv"
     usafact_cases_filename_temp = "usafact_cases_temp.csv"
+    usafact_pop_data_filename = "recovery/covid_county_population_usafacts.csv"
 
     if not test:
         download_document(usafact_cases_data_url, usafact_cases_filename_temp)
@@ -30,7 +37,7 @@ def main():
     with open(usafact_cases_filename_temp, "r") as source:
         reader = csv.reader(source)
         header = next(reader)
-        print(header)
+        # print(header)
         for i in range(len(header)):
             if('/' in header[i]):
                 header[i] = "'" + header[i] + "'"
@@ -50,37 +57,56 @@ Col4=stateFIPS Long
 
             for i in range(4, len(header)):
                 if i == len(header) - 1:
-                    schema.write("Col%d=%s Long\n" % (i+1, header[i].replace(" ", "_")))  
+                    schema.write("Col%d=%s Text Width 8000\n" % (i+1, header[i].replace(" ", "_")))  
                 else:
                     schema.write("Col%d=%s Long\n" % (i+1, header[i])) # .replace("/", "_").replace("'", "_")
 
-        with open(os.path.join(directory, usafact_cases_filename), "w") as result:
+        with open(os.path.join(directory, usafact_cases_filename), "w") as result, open(usafact_pop_data_filename, "r") as pop:
+            pop_data = csv.reader(pop)
+
             writer = csv.writer(result)
             writer.writerow(header)
             for row in reader:
-                # TODO: insert real days of improvement algorithm
-                days = 0
-                for i in range(len(row) -1, 5 + 3, -1):
-                    day = int(row[i])
-                    day_before = int(row[i - 1])
-                    day_two_before = int(row[i - 2])
-                    three_day_slope = (day - day_two_before
-                    if(int(day - day_before < three_day_slope)):
-                        
-                    # if(int(row[i]) < int(row[i-1])):
-                    #     days += 1
-                    # else:
-                    #     break
+
+                moving_average = []
+
+                for i in range(len(row) - 15, len(row) - 1):
+                    moving_average.append((float(row[i]) + float(row[i-1]) + float(row[i-2])) / 3.0)
+
+                two_week_incidence = moving_average[13] - moving_average[0] # sum(moving_average)
+                slope = two_week_incidence / 14.0
+
+                # 6 Categories:
+                # print(moving_average)
+
+                # 5) no cases
+                if(sum(moving_average) > 0):
+                    # 1) sustained decline: if downward slope over 3 day-moving-averages over two weeks
+                    # 4) 0-5 cases in past 2 weeks are considered to meet 14 day downward traj goal.
+                    if(slope <= -.1 or two_week_incidence < 5.0):
+                        days = "significant improvement"
+                    else:
+                        # 2) two-week low incidence <= 10 per 100,000
+                        pop = get_pop(pop_data, row[0])
+                        # print(pop)
+                        # print(10 * pop / 100000.0)
+                        # print(two_week_incidence)
+                        if(two_week_incidence <= 10.0 * pop / 100000.0):
+                            days = "low incidence some improvement"
+                        # 3) two-week high incidence >= 10 per 100,000 and slope -.1 <= x <= .1 for no more than 5 days
+                        elif(two_week_incidence >= pop / 100,000  and slope >= -.1 and slope <= .1):
+                            days = "high incidence some improvement"
+                        # 6) no improvement
+                        else:
+                            days = "no improvement"
+                else:
+                    days = "no cases"
 
                 row.append(str(days))
-
-                # row[0] = "'" + row[0] + "'"
-
                 writer.writerow(row)
-
     
-    # if not test:
-    #     os.remove(usafact_cases_filename_temp)
+    if not test:
+        os.remove(usafact_cases_filename_temp)
 
 if __name__ == "__main__":
     if(len(sys.argv) > 1) and os.path.exists(sys.argv[1]):
